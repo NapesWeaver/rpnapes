d92f7a2281f2ce027ab6d766b9668f8dd34a33ec
Craig Pounds <nweaver@windstream.net>
2020-02-17 20:58:54 -0600

    Removed hideKeyboard function

--- scripts/index.js
+++ scripts/index.js
@@ -1912,37 +1912,6 @@ function toggleKeyboard() {
   }
   $('txtInput').focus();
 }
-// Minimize mobile keyboard
-function hideKeyboard() {
-  //this set timeout needed for case when hideKeyborad
-  //is called inside of 'onfocus' event handler
-  setTimeout(function () {
-
-    //creating temp field
-    var field = document.createElement('input');
-    field.setAttribute('type', 'text');
-    //hiding temp field from peoples eyes
-    //-webkit-user-modify is nessesary for Android 4.x
-    field.setAttribute('style', 'position:absolute; top: 0px; opacity: 0; -webkit-user-modify: read-write-plaintext-only; left:0px;');
-    document.body.appendChild(field);
-
-    //adding onfocus event handler for out temp field
-    field.onfocus = function () {
-      //this timeout of 200ms is nessasary for Android 2.3.x
-      setTimeout(function () {
-
-        field.setAttribute('style', 'display:none;');
-        setTimeout(function () {
-          document.body.removeChild(field);
-          document.body.focus();
-        }, 14);
-
-      }, 200);
-    };
-    //focusing it
-    field.focus();
-  }, 50);
-}
 
 function storeCookie(aName, tmpArray) {
   var d = new Date();
