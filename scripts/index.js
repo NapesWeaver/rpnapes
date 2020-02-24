6b555d9254e4b2ff3baf2bfb1f240977ff1782a9
Craig Pounds <nweaver@windstream.net>
2020-02-24 14:08:54 -0600

    Testing mobile insertAtCursor -  mobileKeyboardAllow()

--- scripts/index.js
+++ scripts/index.js
@@ -95,8 +95,8 @@ function hapticResponse() {
   }
 }
 function mobileKeyboardAllow() {
-  setTimeout(function(){
-    $('txtInput').readOnly = false; }, 300);  
+  $('txtInput').readOnly = false;
+  insertAtCursor($('txtInput'), '');
 }
 
 //////// Buttons /////////////////////////////////////////////////////////////////////
