eba44775249fb49c94d3aca038eca0c3f9979fac
Craig Pounds <nweaver@windstream.net>
2020-02-24 18:05:56 -0600

    Changed to radio stations running over https

--- scripts/index.js
+++ scripts/index.js
@@ -2832,7 +2832,9 @@ function sensor1() {
     $('viewport').src = '';
     playAudio($('keypress7'));
     playAudio($('scanner'));
-    $('viewport').src = 'http://64.40.99.76:8000/http://64.40.99.76:8000';
+    // $('viewport').src = 'http://64.40.99.76:8000/http://64.40.99.76:8000';// 1970's http :(
+    $('viewport').src = 'https://tunein.com/embed/player/s249942/';// Classic Hits
+    // $('viewport').src = 'https://tunein.com/embed/player/s35862/';// KZUM
   }
 }
 function sensor2() {
@@ -2842,7 +2844,9 @@ function sensor2() {
     $('viewport').src = '';
     playAudio($('keypress7'));
     playAudio($('scanner'));
-    $('viewport').src = 'http://64.5.130.47:80/http://64.5.130.47:80';
+    // $('viewport').src = 'http://64.5.130.47:80/http://64.5.130.47:80';// 1920's http :(
+    // $('viewport').src = 'https://tunein.com/embed/player/s35862/';// NET
+    $('viewport').src = 'https://tunein.com/embed/player/s67176/';// BBC World Service
   }
 }
 
