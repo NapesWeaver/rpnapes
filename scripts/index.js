a651b5e52c6e373c7b25183db1d918c6c600f6f6
Craig Pounds <nweaver@windstream.net>
2020-02-25 10:59:54 -0600

    Changed testVar value

--- scripts/index.js
+++ scripts/index.js
@@ -13,7 +13,7 @@ var restores = [33];
 var stackSize = 14;
 var stackFocus = false;
 var fixDecimal = -1;
-var testVar = '10:13AM';
+var testVar = '10:59';
 
 function NumberObject(soul, realPart, units, imaginary, timeStamp) {
 
