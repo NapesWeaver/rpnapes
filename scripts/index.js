39f73d384f78641532a47b6c5ec9870271554744
Craig Pounds <nweaver@windstream.net>
2020-03-04 21:06:44 -0600

    Responsive max-width: 320px

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,7 @@ var stackSize = 14;
 var stackFocus = false;
 var shifted = false;
 var fixDecimal = -1;
-var variable = '20:50:12';
+var variable = '21:5:11';
 
 function NumberObject(soul, realPart, units, imaginary, timeStamp) {
 
