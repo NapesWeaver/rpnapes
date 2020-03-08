5478854672ecc77bb2c022bbcb93bbdb50a15abe
Craig Pounds <nweaver@windstream.net>
2020-03-08 15:55:20 -0500

    Again with testing backspace symbol for mobile

--- scripts/index.js
+++ scripts/index.js
@@ -504,8 +504,8 @@ function btn_shift() {
     $('btnEnter').className = 'btn-big btn-big-font';
     $('btnEnter').style.color = '#0000A0';
     $('btnEnter').value = '=';
-    // $('btnDelete').innerHTML = '<span class="symbol-med">⇐</span>';
-    $('btnDelete').innerHTML = '<span class="symbol-med">🡄</span>';
+    $('btnDelete').innerHTML = '<span class="symbol-med">⇐</span>';
+    // $('btnDelete').innerHTML = '<span class="symbol-med">⬅</span>';
     $('btnInverse').value = '! x';
     $('btnLog').innerHTML = 'log<sub>e</sub>';
     $('btnRoot').innerHTML = '<sup>x</sup>&nbsp;&#8730;¯y';    
