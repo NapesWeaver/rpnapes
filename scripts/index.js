a553f41fba026da94e328dbab33d57811815b60f
Craig Pounds <nweaver@windstream.net>
2020-09-07 17:43:31 -0500

    Disabled parseRoot

--- scripts/index.js
+++ scripts/index.js
@@ -409,6 +409,7 @@ function btn_EE() {
   }
   else {
     if (/(?!^[-+]?\d+[.]?\d*[eE])[-+]?\d+[.]?\d*[eE]?[-+]?\d* *[-+]? *\d*[.]?\d*/g.test($('txtInput').value) && !/.*[eE].*[eE].*/g.test($('txtInput').value)) {
+    //if ((/(?!^[-+]?\d+[.]?\d*[eE])[-+]?\d+[.]?\d*[eE]?[-+]?\d* *[-+]? *\d*[.]?\d*/g.test($('txtInput').value) || /[-+]?[ΦeπGc]/g.test($('txtInput').value)) && !/.*[eE].*[eE].*/g.test($('txtInput').value)) {
       insertAtCursor($('txtInput'), 'e');
     }
   }
@@ -1584,12 +1585,13 @@ function parseEvaluation(input) {
     if (/(?!Math\.a?)sin\(/.test(input)) input = parseTrigs(input, 'sin', Math.asin, Math.sin);
     if (/(?!Math\.a?)cos\(/.test(input)) input = parseTrigs(input, 'cos', Math.acos, Math.cos);
     if (/(?!Math\.a?)tan\(/.test(input)) input = parseTrigs(input, 'tan', Math.atan, Math.tan);  
-    while (/√[(ΦeπGc0-9]/.test(input)) input = parseRoot(input);// nth root -> Math.pow(y, 1/x) eg. Math.pow(25, 1/2) == 5 
+    // while (/√[(ΦeπGc0-9]/.test(input)) input = parseRoot(input);// nth root -> Math.pow(y, 1/x) eg. Math.pow(25, 1/2) == 5 
+    if (/√[(ΦeπGc0-9]/.test(input)) rpnAlert('Function not implemented :(');
     // *add ln(x) -> Math.log(x)
     // *add log(x) -> Math.log(10) / Math.log(x)
     // *add log2(8) = 3 -> log y(x)-> Math.log(y) / Math.log(x)
     // *add ! ->
-  }  
+  }
   return input;
 }
 function parseRoot(input) {
