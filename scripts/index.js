56a31fbb3207f3fae498d7c5e000bf9cd027d4f1
Craig Pounds <nweaver@windstream.net>
2020-11-11 22:34:41 -0600

    Moved autocomplete attribute from lstStack to txtInput ;)

--- scripts/index.js
+++ scripts/index.js
@@ -13,7 +13,7 @@ const e = 2.718281828459045;
 const π = 3.141592653589793;
 const G = 6.674E-11;
 const c = 299792458;
-const tStamp = '22:28:24';
+const tStamp = '22:33:21';
 var testing = false;
 
 var stack = [];
