9486e0afe7047b988160ed421b84e8fde45d5559
Craig Pounds <nweaver@windstream.net>
2020-11-27 16:24:22 -0600

    Added Array.prototype.indexOf() for backward support

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,36 @@
 ﻿var $ = function (id) {
   return document.getElementById(id);
 };
+/*
+  Array.prototype.indexOf()
+  Added to the ECMA-262 standard in the 5th edition may not work in all browsers.
+  You can work around this by utilizing the following code at the beginning of your scripts.
+  This will allow you to use indexOf() when there is still no native support.
+  This algorithm matches the one specified in ECMA-262, 5th edition, assuming TypeError and Math.abs() have their original values.
+  This version tries to optimize by only checking for "in" when looking for undefined and
+  skipping the definitely fruitless NaN search. Other parts are merely cosmetic conciseness.
+*/
+if (!Array.prototype.indexOf)
+  Array.prototype.indexOf = (function(Object, max, min) {
+    'use strict'
+    return function indexOf(member, fromIndex) {
+      if (this === null || this === undefined)
+        throw TypeError('Array.prototype.indexOf called on null or undefined')
+
+      var that = Object(this), Len = that.length >>> 0, i = min(fromIndex | 0, Len)
+      if (i < 0) i = max(0, Len + i)
+      else if (i >= Len) return -1
+
+      if (member === void 0) {        // undefined
+        for (; i !== Len; ++i) if (that[i] === void 0 && i in that) return i
+      } else if (member !== member) { // NaN
+        return -1 // Since NaN !== NaN, it will never be found. Fast-path it.
+      } else                          // all else
+        for (; i !== Len; ++i) if (that[i] === member) return i 
+
+      return -1 // if the value was not found, then return -1
+    }
+  })(Object, Math.max, Math.min)
 
 var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 var isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
