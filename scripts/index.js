e98b9e9c03957766263108956dcea75608c0dec9
Craig Pounds <nweaver@windstream.net>
2020-12-30 23:13:58 -0600

    Restyling hover for YouTube and Google buttons

--- scripts/index.js
+++ scripts/index.js
@@ -43,7 +43,7 @@ const e = 2.718281828459045;
 const π = 3.141592653589793;
 const G = 6.674E-11;
 const c = 299792458;
-const tStamp = '21:34:7';
+const tStamp = '22:28:20';
 var testing = false;
 
 var stack = [];
