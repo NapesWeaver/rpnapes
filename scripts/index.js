f4b765388fe2a48b0a0bdefdccfa1fd8f8967cf1
Craig Pounds <nweaver@windstream.net>
2021-01-01 22:57:16 -0600

    Testing screen size for mobile lst-stack height: 15.6em

--- scripts/index.js
+++ scripts/index.js
@@ -46,7 +46,7 @@ const e = 2.718281828459045;
 const π = 3.141592653589793;
 const G = 6.674E-11;
 const c = 299792458;
-const tStamp = '22:28:20';
+const tStamp = '22:56:48';
 var testing = false;
 
 var stack = [];
