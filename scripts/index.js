de4bc3611f3b238dfb7db1926091e776238da091
Craig Pounds <nweaver@windstream.net>
2021-04-04 16:01:44 -0500

    Testing for mobile - trying $(lst-stack).onclick = mobileKeyboardAllow

--- scripts/index.js
+++ scripts/index.js
@@ -51,7 +51,7 @@ var e = Math.exp(1);// 2.718281828459045
 var π = Math.PI;// 3.141592653589793
 var G = 6.674e-11;
 var c = 299792458;
-var tStamp = '15:52:56';
+var tStamp = '16:01:56';
 var testing = false;
 
 var stack = [];
@@ -2370,7 +2370,7 @@ function mathsRoot(y, x) {
 
 // Wired to HTML
 function lstStackFocus() {
-  // stackFocus = true;  
+  stackFocus = true;  
 }
 
 function txtInputFocus() {
@@ -4381,6 +4381,8 @@ window.onload = function () {
     return false;
   }
   resizeTextarea($('lst-stack'));
+  
+  $('lst-stack').onclick = mobileKeyboardAllow;
   
   // Text Input
   $('txt-input').onclick = mobileKeyboardAllow;
