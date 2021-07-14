615cfbba00264dc7ae52d7f12139cef8c6e8fe9f
Craig Pounds <nweaver@windstream.net>
2021-07-14 14:10:46 -0500

    btnOff() - final testing for mobile!

--- scripts/index.js
+++ scripts/index.js
@@ -51,7 +51,7 @@ var e = Math.exp(1);// 2.718281828459045
 var π = Math.PI;// 3.141592653589793
 var G = 6.674e-11;
 var c = 299792458;
-var tStamp = '14:7:20';
+var tStamp = '14:10:20';
 var testing = false;
 
 var stack = [];
@@ -893,36 +893,20 @@ function pushObjectToStack(tmpArray) {
   stack.push(objY);
 }
 
-// function btnOff() {
-//   monOff();
-//   tricorderOff();  
-//   window.open('','_self').close();
-
-//   window.open(location, '_self').close();
-//   window.close();
-
-//   window.open('', '_self', '');
-//   window.close();
-
-//   window.top.close();
-  
-//   rpnAlert('Scripts may only close windows they opened.');
-//   window.location.href = 'https://www.google.com';
-// }
-
 function btnOff() {
-
   monOff();
-  tricorderOff();
+  tricorderOff();  
+
   window.open('','_self').close();
+  window.open(location, '_self').close();
+  window.close();
   window.open('', '_self', '');
   window.close();
   window.top.close();
-  rpnAlert('Window not opened with window.open()');
-  return false;
-  //throw new Error();
+  
+  rpnAlert('Scripts may only close windows they opened.');
+  window.location.href = 'https://www.google.com';
 }
-
 
 //////// Algebraic Buttons ///////////////////////////////////////////////////////////
 
