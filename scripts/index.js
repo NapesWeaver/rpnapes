1c22b765227021f27b7d4ea3543b1aa1bb572345
Craig Pounds <nweaver@windstream.net>
2021-10-13 20:37:50 -0500

    Trying min-width only for mobile #wrap

--- scripts/index.js
+++ scripts/index.js
@@ -58,7 +58,7 @@ var e = Math.exp(1);// 2.718281828459045
 var π = Math.PI;// 3.141592653589793
 var G = 6.674e-11;
 var c = 299792458;
-var tStamp = '20:25:25';
+var tStamp = '20:36:20';
 var testing = false;
 
 var stack = [];
