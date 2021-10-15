012cec4ad716176d688c59791416796bc133ea42
Craig Pounds <nweaver@windstream.net>
2021-10-14 21:34:38 -0500

    Added responsive borders and padding for 640 and 1680px

--- scripts/index.js
+++ scripts/index.js
@@ -58,7 +58,7 @@ var e = Math.exp(1);// 2.718281828459045
 var π = Math.PI;// 3.141592653589793
 var G = 6.674e-11;
 var c = 299792458;
-var tStamp = '21:02:34';
+var tStamp = '21:33:34';
 var testing = false;
 
 var stack = [];
