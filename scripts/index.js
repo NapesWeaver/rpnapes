89e44fba2becc52ec1b4e194cb9f691e28b11f52
Craig Pounds <nweaver@windstream.net>
2021-10-14 22:09:56 -0500

    Revert again! I should be using branches :(

--- scripts/index.js
+++ scripts/index.js
@@ -58,7 +58,7 @@ var e = Math.exp(1);// 2.718281828459045
 var π = Math.PI;// 3.141592653589793
 var G = 6.674e-11;
 var c = 299792458;
-var tStamp = '21:43:34';
+var tStamp = '22:09:34';
 var testing = false;
 
 var stack = [];
