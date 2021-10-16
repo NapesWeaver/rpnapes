6be7ef82bf2d9b208cf0d25f3eabd0503d502f47
Craig Pounds <nweaver@windstream.net>
2021-10-16 16:34:17 -0500

    Made buttons responsive to veiwport width

--- scripts/index.js
+++ scripts/index.js
@@ -57,7 +57,7 @@ var e = Math.exp(1);// 2.718281828459045
 var π = Math.PI;// 3.141592653589793
 var G = 6.674e-11;
 var c = 299792458;
-var tStamp = '13:59s:22';
+var tStamp = '16:33:22';
 var testing = false;
 
 var stack = [];
