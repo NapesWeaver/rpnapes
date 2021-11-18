1b14f85d794a0c1634c9ea9a9d187832cae52903
Craig Pounds <nweaver@windstream.net>
2021-11-18 00:00:15 -0600

    Code clean-up

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator
 var isFirefox = /firefox/i.test(navigator.userAgent);
 var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 var isMobile = false;
-// Device Detection
+
 if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
 || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
   isMobile = true;
@@ -15,7 +15,6 @@ var isPhone = false;
 if (/mobile/i.test(navigator.userAgent) && !/ipad|tablet/i.test(navigator.userAgent)) {
   isPhone = true;
 }
-
 if (isPhone) navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 
 new ResizeObserver(unFloat).observe($('lst-stack'));
@@ -28,9 +27,9 @@ var ℮ = Math.exp(1);
 var π = Math.PI;
 var ɢ = 6.674e-11;
 var ⅽ = 299792458;
-var inversed = '';
-var tStamp = '19:28:46';
 var testing = false;
+var cashed = '';
+var tStamp = '19:28:46';
 
 var stack = [];
 var backups = [];
@@ -45,7 +44,6 @@ var sciDecimal = -1;
 var radix = 10;
 
 function NumberObject(soul, realPart, imaginary, units) {
-
   this.soul = soul;
   this.realPart = realPart;
   this.imaginary = imaginary;
@@ -78,7 +76,6 @@ NumberObject.prototype.getUnits = function () {
 NumberObject.prototype.toString = function () {
   return this.soul + ', ' + this.realPart + ', ' + this.imaginary + ', ' + this.units;
 };
-
 /**
  * Array.prototype.indexOf()
  * Added to the ECMA-262 standard in the 5th edition may not work in all browsers.
@@ -426,7 +423,7 @@ function btnEnter() {
   }
   updateDisplay();
   parseCommand();
-  inversed = '';
+  cashed = '';
 }
 
 function btnEval() {
@@ -968,7 +965,7 @@ function inverse() {
   var isImaginary = !isNaN(objX.getImaginary());
   var newUnits = inverseUnits(decodeSpecialChar(objX.getUnits()));
 
-  if ($('txt-input').value === inversed && $('txt-input').value !== decodeSpecialChar(backups[backups.length - 3])) {    
+  if ($('txt-input').value === cashed && $('txt-input').value !== decodeSpecialChar(backups[backups.length - 3])) {    
     $('txt-input').value = decodeSpecialChar(backups[backups.length - 3]);
   } else {
     if (isNumber || isImaginary) {
@@ -1003,7 +1000,7 @@ function inverse() {
       }      
     }
   }  
-  if (!/Infinity/g.test(objX.getSoul())) inversed = $('txt-input').value;
+  if (!/Infinity/g.test(objX.getSoul())) cashed = $('txt-input').value;
   $('txt-input').select();
 }
 
@@ -1713,46 +1710,6 @@ function extractSubString(tmpArray) {
   return subString;
 }
 
-/**
- * Get the user IP throught the webkitRTCPeerConnection
- * @param onNewIP {Function} listener function to expose the IP locally
- * @return undefined
- */
-function getUserIP(onNewIP) {
-  // onNewIp - your listener function for new IPs
-  // Compatibility for firefox and chrome
-  var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-  var pc = new myPeerConnection({
-      iceServers: []
-    }),
-    noop = function () { },
-    localIPs = {},
-    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
-    key;
-
-  function iterateIP(ip) {
-    if (!localIPs[ip]) onNewIP(ip);
-    localIPs[ip] = true;
-  }
-  // Create a bogus data channel
-  pc.createDataChannel('');
-  // Create offer and set local description
-  pc.createOffer().then(function (sdp) {
-    sdp.sdp.split('\n').forEach(function (line) {
-      if (line.indexOf('candidate') < 0) return;
-      line.match(ipRegex).forEach(iterateIP);
-    });
-    pc.setLocalDescription(sdp, noop, noop);
-  }).catch(function (e) {
-    // An error occurred, so handle the failure to connect
-  });
-  // Listen for candidate events
-  pc.onicecandidate = function (ice) {
-    if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-    ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
-  };
-}
-
 function insertTime() {
   insertText(getTime());
 }
@@ -1824,16 +1781,6 @@ function geolocationError(error) {
   case error.UNKNOWN_ERROR:
     rpnAlert('An unknown error occurred.');
     break;
-  }
-}
-
-function getIP() {
-  if (/*@cc_on!@*/false || !!document.documentMode) {// IE
-    rpnAlert('Not supported by this browser.');
-  } else {
-    getUserIP(function (ip) {
-      inputText(ip);
-    });
   }
 }
 
