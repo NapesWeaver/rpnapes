759b4497d90f35eb584655f11ca89bace35925ec
NapesWeaver <napesweaver@gmail.com>
2022-04-05 23:27:04 -0500

    Restyled nav links

--- reference/scripts/sandbox.js
+++ reference/scripts/sandbox.js
@@ -6,6 +6,21 @@ function displayOutput() {
     document.getElementById('html-output').contentWindow.document.close();
 }
 
+document.getElementById('html-input').addEventListener('keydown', function(e) {
+    if (e.key === 'Tab') {
+      e.preventDefault();
+      var start = this.selectionStart;
+      var end = this.selectionEnd;  
+      // set textarea value to: text before caret + tab + text after caret
+      this.value = this.value.substring(0, start) + "\t" + this.value.substring(end);  
+      // put caret at right position again
+      this.selectionStart = this.selectionEnd = start + 1;
+    }
+    if (e.key === 'Enter') displayOutput();
+  });
+
 window.onload = function() {
     document.getElementById('html-input').onchange = displayOutput;
+    document.getElementById('html-input').onmouseleave = displayOutput;
+    document.getElementById('html-input').onclick = displayOutput;
 }
