604a3b6721ab387639e707887662fb75d32a5609
NapesWeaver <napesweaver@gmail.com>
2022-04-07 22:02:17 -0500

    Removed hidden space characters from template

--- reference/scripts/sandbox.js
+++ reference/scripts/sandbox.js
@@ -8,12 +8,12 @@ var template = `
     <head>
       <meta charset="UTF-8"/>
       <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1"/>    
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
       <title>Document</title>
       <style>
         
-      </style>        
-    </head>     
+      </style>
+    </head>
     <body>
       
       
