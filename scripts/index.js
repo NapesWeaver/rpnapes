4c19607ddeefd598d4188d52228d50f6e90cbb48
NapesWeaver <napesweaver@gmail.com>
2022-07-11 11:54:29 -0500

    Testing for mobile - moved resizeTextareas() to end of btnXoff()

--- scripts/index.js
+++ scripts/index.js
@@ -47,7 +47,7 @@ var sciDecimal = -1;
 var engDecimal = -1;
 var radix = 10;
 
-var tStamp = '16:10:00';
+var tStamp = '11:50:00';
 var testing = false;
 
 function NumberObject(soul, realPart, imaginary, units) {
@@ -343,8 +343,7 @@ function showTricorder() {
 
 function btnXoff() {
 
-  if (isMobile) setTimeout(resizeTextAreas(), 600);
-
+  
   if ($('rpnapes').classList.contains('hidden')) {
     // Notes is visible - turn on RPNapes
     rpnapesOn();
@@ -352,6 +351,7 @@ function btnXoff() {
     // RPNapes is visible - turn on Notes
     notesOn();
   }
+  if (isMobile) setTimeout(resizeTextAreas(), 300);
 }
 
 function copy() {
