1ec4e21d183202700a239597d5c6a2a2cf0e582f
NapesWeaver <napesweaver@gmail.com>
2022-08-28 18:17:08 -0500

    Persistence pays off - Happy Happy Happy, Joy Joy Joy :) :) :)

--- scripts/index.js
+++ scripts/index.js
@@ -47,7 +47,7 @@ var sciDecimal = -1;
 var engDecimal = -1;
 var radix = 10;
 
-var tStamp = '16:23:00';
+var tStamp = '18:15:00';
 var testing = false;
 
 function NumberObject(soul, realPart, imaginary, units) {
