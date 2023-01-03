130a68f510dd30825d9cd238a8c2a305f1ddbdd9
NapesWeaver <napesweaver@gmail.com>
2023-01-03 10:46:27 -0600

    Excluding units for results that are NaN

--- scripts/index.js
+++ scripts/index.js
@@ -2248,7 +2248,8 @@ function displayResult(result, newUnits) {
     }
     if (objX) result = objToString(objX);
     
-    if (result !== '0') result += newUnits;
+    // if (result !== '0') result += newUnits;
+    if (result !== '0' && !isNaN(result)) result += newUnits;
   
     $('txt-input').value = result;  
     updateDisplay();
@@ -2270,8 +2271,8 @@ function displayResults(results, newUnits) {
     }
     if (objX) results[i] = objToString(objX);
     $('txt-input').value += results[i];
-
-    if (results[i] !== '0') $('txt-input').value += newUnits;
+    
+    if (results[i] !== '0' && !isNaN(results[i])) $('txt-input').value += newUnits;
 
     if (i < results.length - 1) $('txt-input').value += '\n';
   }
