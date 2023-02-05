43decb031f551d07a758d35f39c43aac064ed2f9
NapesWeaver <napesweaver@gmail.com>
2023-02-04 20:37:17 -0600

    Replacing ohm with Ω while extracting units

--- scripts/index.js
+++ scripts/index.js
@@ -4397,6 +4397,7 @@ function extractUnits(tmpString) {
   } else {
     tmpUnits += tmpString.match(/(?![eE][-+]?[0-9]+)(?![a-f0-9]+[ij]*\b)(?![ij]\b)(?:[1][/])?[Ω♥a-zA-Z]+[-*^Ω♥a-zA-Z.0-9/]*$/);    
   }
+  tmpUnits = tmpUnits.replace(/(?<!\w)ohm(?!\w)/g, 'Ω');
   return tmpUnits;
 }
 
