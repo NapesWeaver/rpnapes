6375342439ec2446bf3396cfe0dd26d3ea45dedb
NapesWeaver <napesweaver@gmail.com>
2023-06-24 16:13:49 -0500

    Testing for mobile

--- scripts/index.js
+++ scripts/index.js
@@ -51,7 +51,7 @@ var engDecimal = -1;
 var radix = 10;
 var currency = '';
 
-var tStamp = '14:34:00';
+var tStamp = '16:13:00';
 var testing = false;
 
 function NumberObject(soul, realPart, imaginary, units) {
@@ -450,6 +450,9 @@ function hapticResponse() {
   if (isMobile) {
     haptic();
     $('txt-input').readOnly = true;
+    setTimeout(function() {
+      $('txt-input').readOnly = false;
+    }, 1000);
   }
 }
 
