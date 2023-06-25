8549e328c4f42ffc12dea560189d5d020fe53e7a
NapesWeaver <napesweaver@gmail.com>
2023-06-25 12:12:46 -0500

    Testing for Firefox mobile

--- scripts/index.js
+++ scripts/index.js
@@ -51,7 +51,7 @@ var engDecimal = -1;
 var radix = 10;
 var currency = '';
 
-var tStamp = '12:09:00';
+var tStamp = '12:11:00';
 var testing = false;
 
 function NumberObject(soul, realPart, imaginary, units) {
@@ -467,9 +467,9 @@ function hapticResponse() {
       }, 100);
     } else {
       $('txt-input').inputMode='none';
+      $('txt-input').readOnly = false;
       setTimeout(function() {
         $('txt-input').inputMode='text';
-        $('txt-input').readOnly = false;
       }, 1000);
     }
   }
