a855efd9309f060f725ea15b876e364d99a478a4
NapesWeaver <napesweaver@gmail.com>
2023-06-25 12:31:06 -0500

    Testing for mobile

--- scripts/index.js
+++ scripts/index.js
@@ -51,7 +51,7 @@ var engDecimal = -1;
 var radix = 10;
 var currency = '';
 
-var tStamp = '12:24:00';
+var tStamp = '12:30:00';
 var testing = false;
 
 function NumberObject(soul, realPart, imaginary, units) {
@@ -450,6 +450,13 @@ function toggleHaptic() {
 //   if (isMobile) {
 //     haptic();
 //     $('txt-input').readOnly = true;
+//   }
+// }
+
+// function hapticResponse() {
+//   if (isMobile) {
+//     haptic();
+//     $('txt-input').readOnly = true;
 //     if (!isFirefox) setTimeout(function() {
 //       $('txt-input').readOnly = false;
 //     }, 100);    
@@ -460,12 +467,10 @@ function hapticResponse() {
   if (isMobile) {
     haptic();
     $('txt-input').readOnly = true;
-    if (!isFirefox) {
+    if (!isFirefox && !$('menu-keyboard-li').classList.contains('strikethrough')) {
       setTimeout(function() {
         $('txt-input').readOnly = false;
       }, 100);    
-    } else {
-      $('txt-input').readOnly = false;
     }
   }
 }
