21d3bcc8da8efb25e9402620e0d6b54a35cee8d3
NapesWeaver <napesweaver@gmail.com>
2024-07-16 17:19:34 -0500

    Revert :(

--- scripts/index.js
+++ scripts/index.js
@@ -53,7 +53,7 @@ var engDecimal = -1;
 var radix = 10;
 var currency = '';
 
-var tStamp = '5:11:00';
+var tStamp = '5:19:00';
 var testing = false;
 
 function NumberObject(soul, realPart, imaginary, units) {
@@ -6233,11 +6233,6 @@ window.onload = function () {
   }  
   
   $('menu-help').onclick = menuHelp;
-
-  // if (isFirefox && isMobile) {
-  //   toggleKeyboard();
-  //   resizeTextAreas();
-  // } 
 
   if (isMobile) {
     $('menu-twig').style = 'display:none';
