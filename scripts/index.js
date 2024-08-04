70a8cac59784a74b0c595fa40cc10634587d74f7
NapesWeaver <napesweaver@gmail.com>
2024-08-04 15:28:22 -0500

    Appending ?udm=14 to Google queries

--- scripts/index.js
+++ scripts/index.js
@@ -1279,20 +1279,21 @@ function searchGoogle() {
   var domainString = 'https://www.google.com/search?q=';
   
   if (query) {
+
     if (/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(:([0-9]|[1-9][0-9]{1,3}|[1-3][0-9]{4}|4[0-8][0-9]{3}|490[0-9]{2}|491[0-4][0-9]|4915[01]))?$/.test(query)
     || /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/.test(query)
     || /.+\.(au|biz|blog|ca|com|de|edu|gov|in|io|mil|net|org|ru|stream|uk|us)\/?.*$/.test(query)
     || /^http[s]?:\/\//.test(query)) {
-
+      
       if (!/^http[s]?:\/\//.test(query)) query = 'http://' + query;
       
       window.open(query);
     } else {
-      domainString += query;
+      domainString += query + '&udm=14';
       window.open(domainString, '_blank');
     }    
   } else {
-    window.open('https://www.google.com/?udm=14');
+    window.open('https://www.google.com/');
   }
 }
 
@@ -1651,7 +1652,7 @@ function btnOff() {
   }
   
   rpnAlert('Scripts may only close windows they opened.');
-  window.location.href = 'https://www.google.com/?udm=14';
+  window.location.href = 'https://www.google.com/';
 }
 
 //////// Algebraic Buttons ///////////////////////////////////////////////////////////
